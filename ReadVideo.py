975d11fb03ab238624482c7f8605f9dadb9770a7
Devreese Jorik <jorik.devreese@student.howest.be>
2022-05-31 13:03:08 +0200

    paths are customisable

--- ReadVideo.py
+++ ReadVideo.py
@@ -3,19 +3,21 @@ import random
 import pathlib 
 import os
 
+source_path = "./SourceFiles"
+image_path = f"{source_path}/Images"
 
-def get_all_frames():
-  vidcap = cv2.VideoCapture('./SourceFiles/videoclip_963155.mp4')
+def get_all_frames(clip ="videoclip_963155"):
+  vidcap = cv2.VideoCapture(f'{source_path}/{clip}.mp4')
   success,image = vidcap.read()
   count = 0
   while success:
-    cv2.imwrite("./SourceFiles/Images/interessant/1frame%d.jpg" % count, image)     # save frame as JPEG file      
+    cv2.imwrite(f"{image_path}/frame{count}.png", image)     # save frame as JPEG file      
     success,image = vidcap.read()
-    print('Read a new frame: ', success)
+    print(f'Read a new frame {count}: {success}')
     count += 1
 
 def get_random_frames(start_video=96,interval=24,random_offset=6):
-  vidcap = cv2.VideoCapture('./SourceFiles/cam6_footage.mp4')
+  vidcap = cv2.VideoCapture(f'{source_path}/cam6_footage.mp4')
   success,image = vidcap.read()
   count = 0
   randomv = random.randint(interval-random_offset,interval+random_offset)
@@ -30,24 +32,24 @@ def get_random_frames(start_video=96,interval=24,random_offset=6):
   while success:
     if count%randomv == 0:
       print(f"added image {count}")
-      cv2.imwrite(f"./SourceFiles/Images/variated/randomframe{count}.jpg", image)     # save frame as JPEG file
+      cv2.imwrite(f"{image_path}/variated/randframe{count}.png", image)     # save frame as JPEG file
       randomv = random.randint(count + interval-random_offset,count + interval+random_offset)     
     success,image = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
 
 def get_cropped_images(cam=6,cropped_under_line=True):
-  vector = [{4:{"x":220,"y":680,"w":1700,"h":1080},6:{"x":380,"y":632,"w":1600,"h":1080}},{4:{"x":380,"y":908,"w":1600,"h":1080},6:{"x":420,"y":908,"w":1500,"h":1080}}][cropped_under_line][cam]
-  temp_files = os.listdir("./SourceFiles/Images")
-  files = []
+  vector = [{4:{"x":220,"y":680,"w":1700,"h":1080},6:{"x":380,"y":632,"w":1600,"h":1080}},{4:{"x":380,"y":908,"w":1600,"h":1080},6:{"x":420,"y":892,"w":1500,"h":1080}}][cropped_under_line][cam]
+  temp_files = os.listdir(f"{image_path}/goals")
   for file in temp_files:
-    if file[-4:] == ".jpg":
-      files.append(file)
-      image = cv2.imread(f"./SourceFiles/Images/{file}")
+    if file[-4:] == ".png":
+      image = cv2.imread(f"{image_path}/{file}")
       cropped_image = image[vector["y"]:vector["h"],vector["x"]:vector["w"]]
       # cv2.imshow("Cropped", cropped_image)
       # cv2.waitKey(0)
-      cv2.imwrite(f"./SourceFiles/Images/cropped/{file}", cropped_image)     # save frame as JPEG file
-  #image = cv2.imread(r"C:\Users\HP\OneDrive\Desktop\<image>.png")
+      cv2.imwrite(f"{image_path}/cropped/{file[:-4]}.png", cropped_image)     # save frame as JPEG file
+      print(f"cropped image: {file[:-4]}")
 
+#get_all_frames("videoclip_977455")
+# get_all_frames("cam6_footage")
 get_cropped_images()
